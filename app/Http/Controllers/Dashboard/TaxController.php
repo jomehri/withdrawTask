ec6ec574d48150f2df5bcdbab52062613289d1c4
aliJo <ali.jomehri@chapagha.com>
2022-03-08 00:24:10 +0330

    initial commit

---
+++ app/Http/Controllers/Dashboard/TaxController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Dashboard;
+
+use App\Http\Controllers\Controller;
+
+class TaxController extends Controller
+{
+	/**
+	 * Calculations of withdrawal and deposit taxes
+	 *
+	 * @return void
+	 */
+	public function index()
+	{
+		dd('its working...');
+	}
+}
